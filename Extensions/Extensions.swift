dfa853f0fc6b44294e6ff3135b4af6a1c00f5170
Raquel Rahmey <rrahmey@aol.com>
2017-02-09 15:07:33 -0500

    few done

---
+++ Extensions/Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  Extensions.swift
+//  Extensions
+//
+//  Created by Raquel Rahmey on 2/9/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func whisper(){
+        self.lowercased()
+    }
+    
+    func shout(){
+        self.uppercased()
+    }
+    
+    func pigLatin() {
+        
+    }
+    
+}
+
